774b6940c46b0f77e5943df45b1a23c3ee614a9c
Dave Ranjan <dtiwari18@gmail.com>
2017-08-06 03:06:25 +0530

    Basic View with Meteor

---
+++ imports/ui/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+
+  render(){
+
+    return(
+      <h1> I am in App.jsx (find me at /imports/ui/App.jsx) </h1>
+    );
+  };
+}
+
+export default App;
